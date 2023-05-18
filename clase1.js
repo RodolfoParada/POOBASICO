5a7b636d0b07a0ff6cfb0e72c54e6d1e68703b46
Rodolfo <rodolfo.parada.gonzalez@gmail.com>
2023-05-17 23:01:21 -0400

    programacion a objetos Javascript

---
+++ clase1.js
@@ -0,0 +1,63 @@
+// objeto literal
+
+const natalia = {
+    name: "Natalia",
+    age: 20,
+    cursosAprobados: [
+        "Curso Definitivo de HTML y CSS",
+        "Curso Práctico de HTML y CSS",
+    ],
+    aprobarCurso(nuevoCursito) {
+        this.cursosAprobados.push(nuevoCursito);
+    },
+};
+
+function Student(name, age, cursosAprobados) {
+    this.name = name;
+    this.age = age;
+    this.cursosAprobados = cursosAprobados;
+    // this.aprobarCurso = function (nuevoCursito) {
+    //   this.cursosAprobados.push(nuevoCursito);
+    // }
+}
+
+//prototipos
+
+Student.prototype.aprobarCurso = function (nuevoCursito) {
+    this.cursosAprobados.push(nuevoCursito);
+}
+
+const juanita = new Student(
+    "Juanita Alejandra",
+    15,
+    [
+        "Curso de Introducción a la Producción de Videojuegos",
+        "Curso de Creación de Personajes",
+    ],
+);
+
+// Prototipos con la sintasis de clases
+class Student2 {
+    constructor({
+        name,
+        cursosAprobados = [],
+        age,
+        email,
+    }) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+        this.cursosAprobados = cursosAprobados;
+    }
+
+    aprobarCurso(nuevoCursito) {
+        this.cursosAprobados.push(nuevoCursito);
+    }
+}
+
+//objeto del constructor
+const miguelito = new Student2({
+    name: "Miguel",
+    age: 28,
+    email: "miguelito@platzi.com",
+});
